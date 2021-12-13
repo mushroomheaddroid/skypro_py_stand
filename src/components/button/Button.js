e587dc33350f9ef68b2bdef29e82768fae54ee44
Akhmetov <alex@akhmetov.net>
2021-12-13 09:33:29 +0300

    v0.1.2 updates

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,12 +1,15 @@
 import styles from "./Button.module.scss";
 import cn from "classnames";
+import Loader from "../loader/Loader";
 
-export const Button = ({ type, label, onClick }) => {
+export const Button = ({ type, label, loading, onClick }) => {
   return (
     <button
-      className={cn(styles.Button, type === "dark" ? styles.Button_type_dark : styles.Button_type_light)}
+      className={cn(styles.Button, type === "dark" ? styles.Button_type_dark : styles.Button_type_light, loading && styles.Button_type_transparent)}
       onClick={onClick}
+      disabled={loading}
     >
+      {loading && <Loader />}
       {label}
     </button>
   )
